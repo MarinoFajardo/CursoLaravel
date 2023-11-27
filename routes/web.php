098dcac30db9f4f710aeacd417d5ed588fa2e5ff
MarinoFajardo <marinofajardo@correo.ugr.es>
2023-11-27 20:47:59 +0100

    Integración de breeze y talwind

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\Gestion\CategoriaController;
-use Illuminate\Support\Facades\Route;
-//Para referenciar otras clases hay que añadir su namespace.
 use App\Http\Controllers\Gestion\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,38 +16,26 @@ use App\Http\Controllers\Gestion\PostController;
 |
 */
 
-/* Ruta a la que se accede cuando se carga la página principal.
-http://example-app.test/
-*/
 Route::get('/', function () {
     return view('welcome');
 });
 
-/**
- * Rutas para el controlador Post de gestión.
- */
-Route::resource('post', PostController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-/**
- * Rutas para el controlador Categorías de Gestión.
- */
-Route::resource('categoria',CategoriaController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-/*
-Haciendo la ruta anterior no es necesario añadir todo esto pero se puede 
-hacer de las dos formas aunque la primera es más cómoda.
-//Ruta para la función index del controlador.
-Route::get('post',[PostController::class,'index']);
-//Ruta para la función show del controador.
-Route::get('post/{post}',[PostController::class,'show']);
-//Ruta para la función create del controador.
-Route::get('post/create',[PostController::class,'create']);
-//Ruta para la función edit del controador.
-Route::get('post/{post}/edit',[PostController::class,'edit']);
-//Ruta para la función store del controlador.
-Route::post('post',[PostController::class,'store']);
-//Ruta para la función update del controlador.
-Route::put('post/{post}',[PostController::class,'update']);
-//Ruta para la función destroy del controlador.
-Route::delete('post/{post}',[PostController::class,'delete']);
-*/
+Route::group(['prefix' => 'gestion'],function(){
+    Route::resources([
+        'post' => PostController::class,
+        'categoria' => CategoriaController::class
+    ]);
+    
+});
+
+require __DIR__.'/auth.php';
